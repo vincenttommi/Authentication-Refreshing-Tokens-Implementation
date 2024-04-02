1b696d96ab3fa01d6e6155995604dd402e9f4e1f
vincenttommi <vincenttommi@gmail.com>
2024-04-03 00:53:58 +0300

    Blacklisting Refresh Tokens

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 
 urlpatterns = [
     path('', views.getRoutes),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
    
 ]
 
